f953e4d68d50b7d072a9218aecaf9c6cef670e71
Milan Parikh <milanmp98@gmail.com>
2017-06-05 16:11:20 -0400

    Login/Registration page and Settings page completed

--- app/src/main/java/com/milanparikh/hotelmonitor/MainActivity.java
+++ app/src/main/java/com/milanparikh/hotelmonitor/MainActivity.java
@@ -1,14 +1,34 @@
 package com.milanparikh.hotelmonitor;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
 public class MainActivity extends AppCompatActivity {
+
+    String serverURL;
+    String appID;
+    EditText usernameEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +36,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.login_toolbar);
         setSupportActionBar(toolbar);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        usernameEditText = (EditText)findViewById(R.id.username_edittext);
+        passwordEditText = (EditText)findViewById(R.id.password_edittext);
+        Button loginButton = (Button)findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                serverURL = sharedPref.getString("server_preference","");
+                appID = sharedPref.getString("app_id_preference","");
+
+                Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                        .applicationId(appID)
+                        .server(serverURL)
+                        .build()
+                );
+                ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Toast.makeText(MainActivity.this, user.getUsername()+ " successfully logged In", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Log in failed: " + e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        Button registerbutton = (Button)findViewById(R.id.register_button);
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                serverURL = sharedPref.getString("server_preference","");
+                appID = sharedPref.getString("app_id_preference","");
+
+                Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                        .applicationId(appID)
+                        .server(serverURL)
+                        .build()
+                );
+                ParseUser user = new ParseUser();
+                user.setUsername(usernameEditText.getText().toString());
+                user.setPassword(passwordEditText.getText().toString());
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e==null) {
+                            Toast.makeText(MainActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Registration failed: " + e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
