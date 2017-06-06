b2c08563436b798ed3f57075f080151617a697b9
Milan Parikh <milanmp98@gmail.com>
2017-06-06 10:58:29 -0400

    Added Live Query feature to basic Master room list

--- app/src/main/java/com/milanparikh/hotelmonitor/Master/Master.java
+++ app/src/main/java/com/milanparikh/hotelmonitor/Master/Master.java
@@ -6,11 +6,15 @@ import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
 import com.milanparikh.hotelmonitor.R;
+import com.parse.Parse;
+import com.parse.ParseLiveQueryClient;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
+import com.parse.SubscriptionHandling;
 
 public class Master extends AppCompatActivity {
+    ParseQuery query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +23,25 @@ public class Master extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.master_toolbar);
         setSupportActionBar(toolbar);
 
-        ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(this, new ParseQueryAdapter.QueryFactory<ParseObject>() {
+        ParseLiveQueryClient parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient();
+
+        final ParseQueryAdapter<ParseObject> adapter = new ParseQueryAdapter<ParseObject>(this, new ParseQueryAdapter.QueryFactory<ParseObject>() {
             @Override
             public ParseQuery<ParseObject> create() {
-                ParseQuery query = new ParseQuery("RoomList");
+                query = new ParseQuery("RoomList");
                 query.orderByDescending("room");
                 return query;
             }
         });
+
+        SubscriptionHandling<ParseObject> subscriptionHandling = parseLiveQueryClient.subscribe(query);
+        subscriptionHandling.handleEvents(new SubscriptionHandling.HandleEventsCallback<ParseObject>() {
+            @Override
+            public void onEvents(ParseQuery<ParseObject> query, SubscriptionHandling.Event event, ParseObject object) {
+                adapter.loadObjects();
+            }
+        });
+
         adapter.setTextKey("room");
         adapter.setObjectsPerPage(60);
 
