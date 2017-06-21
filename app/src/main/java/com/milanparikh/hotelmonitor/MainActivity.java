48649ed1850d432cf56aa6b41de7e44f7dc7524c
Milan Parikh <milanmp98@gmail.com>
2017-06-20 20:45:07 -0400

    HotelMonitor-2 Add auto update functionality Beta version of update function added

--- app/src/main/java/com/milanparikh/hotelmonitor/MainActivity.java
+++ app/src/main/java/com/milanparikh/hotelmonitor/MainActivity.java
@@ -1,8 +1,12 @@
 package com.milanparikh.hotelmonitor;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,11 +21,15 @@ import android.widget.Toast;
 import com.milanparikh.hotelmonitor.Client.Client;
 import com.milanparikh.hotelmonitor.Master.Master;
 import com.milanparikh.hotelmonitor.Master.MasterSetup;
+import com.milanparikh.hotelmonitor.Other.DownloadUpdate;
 import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
     EditText usernameEditText;
     EditText passwordEditText;
     public ParseUser user;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -152,8 +164,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(settingsIntent);
                 return true;
             case R.id.action_update:
-                //Todo: Add update functionality
-                //test
+                updateFunction();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -161,7 +172,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updateFunction() {
+        verifyStoragePermissions(this);
+        DownloadUpdate downloadUpdate = new DownloadUpdate(getApplicationContext());
+        downloadUpdate.execute();
+    }
 
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/milanparikh/hotelmonitor/Other/DownloadUpdate.java
@@ -0,0 +1,85 @@
+package com.milanparikh.hotelmonitor.Other;
+
+import android.content.Context;
+import android.content.Intent;
+import android.icu.util.Output;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by milan on 6/20/2017.
+ */
+
+public class DownloadUpdate extends AsyncTask {
+    File externalStorage;
+    Context context;
+
+    public DownloadUpdate(Context context){
+        this.context = context.getApplicationContext();
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        int count;
+        try {
+            URL url = new URL("http://www.openhouseguest.com/apk-release.apk");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.connect();
+
+            int fileSize = urlConnection.getContentLength();
+            externalStorage = Environment.getExternalStorageDirectory();
+
+            InputStream input = new BufferedInputStream(url.openStream());
+            OutputStream output = new FileOutputStream(externalStorage+"/update.apk");
+
+            byte[] buffer = new byte[1024];
+            float total=0;
+
+            while ((count = input.read(buffer)) != -1) {
+                total += count;
+                publishProgress(""+(int)((total*100)/fileSize));
+                output.write(buffer, 0, count);
+            }
+
+            output.flush();
+            output.close();
+            input.close();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+        Intent install = new Intent().setDataAndType(Uri.parse(externalStorage+"/update.apk"), "application/vnd.android.package-archive");
+        install.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(install);
+    }
+
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+    }
+}
